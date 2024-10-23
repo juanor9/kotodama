1e3813947b3e74c17c1f3a076ad12ca01e38243d
Juan Camilo Orjuela <orjuela9@gmail.com>
2024-10-23 18:28:09 -0500

    improve options for main navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import { User } from "firebase/auth";
 import CurrencyDisplay from "./UiComponents/CurrencyDisplay/CurrencyDisplay.tsx";
 
 interface HeaderProps {
@@ -7,22 +6,19 @@ interface HeaderProps {
   onNavigate: (
     screen: "main" | "summon" | "characters" | "battle" | "profile",
   ) => void;
-  user: User | null;
 }
 
-function Header({ coins, essence, onNavigate, user }: HeaderProps) {
+function Header({ coins, essence, onNavigate }: HeaderProps) {
   return (
     <header className="header">
+      <button
+        type="button"
+        className="profile-btn"
+        onClick={() => onNavigate("main")}
+      >
+        Main
+      </button>
       <CurrencyDisplay coins={coins} essence={essence} />
-      {user && (
-        <button
-          type="button"
-          className="profile-btn"
-          onClick={() => onNavigate("profile")}
-        >
-          Profile
-        </button>
-      )}
     </header>
   );
 }
